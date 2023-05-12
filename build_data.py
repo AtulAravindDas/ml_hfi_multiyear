fixed major bug in the tile line-up, added dense weighting

build_data.py
@@ -105,6 +105,8 @@ def make_sample_list(settings, evaluate_all=False):
             else:
                 sampling_weights = np.ones(np.shape(hfi))
 
+            denseweight_dist = methods.get_denseweight_dist(settings, hfi)
+
             # SHUFFLE TOGETHER and BATCH BY EQUAL YEAR
             # <TO DO> will need to batch by equal tile at a later date
             sample_years = np.asarray(settings["training_years"], dtype=int)
@@ -129,7 +131,7 @@ def make_sample_list(settings, evaluate_all=False):
             # PRINT SIZES
             print(f"{ntrain = }, {nval = }")
 
-            return tagyear_train, taglat_train, taglon_train, tagyear_val, taglat_val, taglon_val
+            return tagyear_train, taglat_train, taglon_train, tagyear_val, taglat_val, taglon_val, denseweight_dist
 
 
 def save_predictions_tif(settings, hfi_predict, predictions_filename):
@@ -164,7 +166,7 @@ def save_predictions_tif(settings, hfi_predict, predictions_filename):
 
     hfi_labels = np.reshape(hfi_labels, (height, width), order="C")
     hfi_predict = np.reshape(hfi_predict, (height, width), order="C")
-    hfi_predict = np.asarray(np.round(hfi_predict * 100), dtype="uint8")
+    hfi_predict = np.asarray(np.round(hfi_predict), dtype="uint8")
     hfi_predict = np.where(hfi_mask == 1, hfi_predict, 255)  # remove ocean and turn to nan
 
     meta_data = {}
@@ -208,15 +210,26 @@ class data_generator:
             for isample in np.arange(0, len(years)):
                 ilat, ilon = input_tiff.index(sample_lons[isample], sample_lats[isample])
 
-                ilat0, ilat1 = ilat[0] - scene_width / 3 * 2 + 1, ilat[0] + scene_width / 3
-                ilon0, ilon1 = ilon[0] - scene_width / 3, ilon[0] + scene_width / 3 * 2 - 1
+                # WRONG
+                # ilat0, ilat1 = ilat[0] - scene_width / 3 * 2 + 1, ilat[0] + scene_width / 3
+                # ilon0, ilon1 = ilon[0] - scene_width / 3, ilon[0] + scene_width / 3 * 2 - 1
+
+                # CORRECT - MAYBE
+                # ilat0, ilat1 = ilat[0] - scene_width / 3, ilat[0] + scene_width / 3 * 2 - 1
+                # ilon0, ilon1 = ilon[0] - scene_width / 3, ilon[0] + scene_width / 3 * 2 - 1
+
+                ilat0, ilat1 = ilat[0] - scene_width / 3 * 2, ilat[0] + scene_width / 3 - 1
+                ilon0, ilon1 = ilon[0] - scene_width / 3 * 2, ilon[0] + scene_width / 3 - 1
 
                 window = Window.from_slices((ilat0, ilat1 + 1), (ilon0, ilon1 + 1))
                 input_scene = np.transpose(input_tiff.read(channels, window=window), axes=(1, 2, 0))
 
                 # add noise to de-noise
-                random_noise = rng.integers(-self.settings["input_noise"], self.settings["input_noise"] + 1, size=1)
-                batch_input[isample, :, :, :] = (input_scene + random_noise) / 255.
+                if self.settings["training"]:
+                    random_noise = rng.integers(-self.settings["input_noise"], self.settings["input_noise"] + 1, size=1)
+                    batch_input[isample, :, :, :] = (input_scene + random_noise)
+                else:
+                    batch_input[isample, :, :, :] = input_scene
 
         # convert to tensor
         dat = tf.convert_to_tensor(batch_input)
@@ -242,13 +255,32 @@ class data_generator:
                 window = Window(ilon[0], ilat[0], 1, 1)
 
                 output_mask = output_tiff.read_masks(1, window=window) // 255.  # convert to 0/1, with 0 = no data
-                batch_output[isample] = output_mask * output_tiff.read(1, window=window) / 100.
+                batch_output[isample] = output_mask * output_tiff.read(1, window=window)
 
                 # this is where we can force the network to predict zeros
-                if batch_output[isample] == 0.:
-                    batch_output[isample] = self.settings["kluge_value_for_zero"]
+                if self.settings["training"]:
+                    if batch_output[isample] == 0.:
+                        batch_output[isample] = self.settings["kluge_value_for_zero"]
+
+                # batch_output[isample] = read_output_data(self, output_tiff, sample_lons[isample], sample_lats[isample])
 
         # convert to tensor
         dat = tf.convert_to_tensor(batch_output)
 
         return dat
+
+
+def read_output_data(self, tiff, sample_lon, sample_lat):
+
+    ilat, ilon = tiff.index(sample_lon, sample_lat)
+    window = Window(ilon[0], ilat[0], 1, 1)
+
+    output_mask = tiff.read_masks(1, window=window) // 255.  # convert to 0/1, with 0 = no data
+    sample_output = output_mask * tiff.read(1, window=window)
+
+    # this is where we can force the network to predict zeros
+    if self.settings["training"]:
+        if sample_output == 0.:
+            sample_output = self.settings["kluge_value_for_zero"]
+
+    return sample_output

methods.py
@@ -8,31 +8,52 @@ permute_shuffle_sample_list(settings, sample_years, sample_lats, sample_lons)
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
+import tensorflow as tf
 
 __author__ = "Elizabeth A. Barnes and Randal J. Barnes"
 __date__ = "11 May 2023"
 
 
-def get_sample_weights(settings, data):
+def get_denseweight_dist(settings, data):
+    # see Steininger et al. (2021)
+    # https://link.springer.com/article/10.1007/s10994-021-06023-5
 
+    epsilon = 0.001
     alpha = settings["sample_weights_alpha"]
-    epsilon = .0001
+    x_values = np.arange(0, 101, 1)
 
-    hist = np.histogram(data, bins=100)
+    hist = np.histogram(data, bins=x_values)
     hist_dist = scipy.stats.rv_histogram(hist, density=True)
+    hist_dist = hist_dist.pdf(x_values)
 
-    weights = np.maximum(1 - alpha * hist_dist.pdf(data), epsilon)
-    weights = weights / np.mean(weights)
+    denseweight_dist = np.maximum(1. - alpha * hist_dist, epsilon)
+    denseweight_dist = denseweight_dist / np.mean(denseweight_dist)
 
-    # plt.figure()
-    # x_values = np.arange(0, 1.01, .01)
-    # plot_weights = np.maximum(1 - alpha * hist_dist.pdf(x_values), epsilon)
-    # plot_weights = plot_weights / np.mean(plot_weights)
-    # n, bins, __ = plt.hist(data, np.arange(0, 1.01, .01), density=True)
-    # plt.plot(x_values, plot_weights)
-    # plt.ylim(0, None)
+    return denseweight_dist
 
-    return weights
+
+def dw_calculator(denseweight_dist, data):
+
+    # scaled_data = tf.cast(tf.math.round(100. * data), dtype=tf.int32)
+    scaled_data = tf.cast(tf.math.round(data), dtype=tf.int32)
+    return tf.gather(denseweight_dist, scaled_data)
+
+
+class DenseWeight_Loss(tf.keras.losses.Loss):
+    def __init__(self, denseweight_dist):
+        super().__init__()
+        self.denseweight_dist = denseweight_dist
+
+    def call(self, y_true, y_pred):
+
+        loss = tf.math.squared_difference(y_true, y_pred)
+
+        weights = dw_calculator(self.denseweight_dist, y_true)
+        loss = tf.multiply(loss, weights)
+
+        loss = tf.reduce_mean(loss)
+
+        return tf.sqrt(loss)
 
 
 def permute_shuffle_sample_list(settings,

train_model.py
@@ -11,6 +11,7 @@ import numpy as np
 import silence_tensorflow.auto
 import tensorflow as tf
 import random
+import methods
 
 __author__ = "Elizabeth A. Barnes and Randal J. Barnes"
 __version__ = "05 May 2023"
@@ -26,7 +27,7 @@ def scheduler(epoch, learning_rate):
         return learning_rate * tf.math.exp(-0.1)
 
 
-def train_model(settings, model, tfds_train, tfds_val):
+def train_model(settings, model, tfds_train, tfds_val, denseweight_dist):
 
     # SET RANDOM SEEDS AGAIN FOR MODEL TRAINING
     np.random.seed(settings["rng_seed"])
@@ -69,8 +70,8 @@ def train_model(settings, model, tfds_train, tfds_val):
     optimizer = tf.keras.optimizers.Adam(settings["learning_rate"])
 
     # loss function
-    # WE WILL WANT TO MODIFY THIS TO INCLUDE THE KLUGE TO ZERO ******
-    loss = tf.keras.losses.MeanSquaredError()
+    # loss = tf.keras.losses.MeanSquaredError()
+    loss = methods.DenseWeight_Loss(tf.constant(denseweight_dist, dtype="float32"))
 
     # SET PRE-FETCH ON DATA
     tfds_train = tfds_train.prefetch(tf.data.AUTOTUNE)