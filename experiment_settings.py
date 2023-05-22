157d732f75a8d1e499446a91549ae7d5c4f1cbea
Elizabeth Barnes <eabarnes@colostate.edu>
2023-05-22 11:34:28 -0600

    getting ready to test losses

--- experiment_settings.py
+++ experiment_settings.py
@@ -94,6 +94,43 @@ def get_settings(experiment_name):
             "save_best_only": True,
         },
 
+        "exp2": {
+            "mode": None,  # "training" or "inference" as set in the code itself
+            "training_years": (2015, 2016, 2017, 2018, 2019),  # (2000, 2005, 2010, 2013)
+            "inference_years": (2020, ),
+
+            "training_region": (-90, 90, -180, 180),  # (lat_south, lat_north, lon_west, lon_east)
+            "inference_region": (-90, 90, -180, 180),  # (lat_south, lat_north, lon_west, lon_east)
+
+            "channels": (1, 2, 3, 4, 5, 6),  # indexing starts at 1, channel 7 = WATER_MASK
+            "scene_width": 11,  # in units of HFI, must be ODD. 30, 50, 70, 90, 110, 130
+            "nbatches": (750, 25),  # (training_batches, validation_batches per landsat tile)
+            "batches_per_epoch": 750,
+            "rng_seed": 33,
+
+            "layers_units": (64, 128, 128, 128, 128),
+            "kernel_size": 3,
+            "max_pool_stride": (2, 2),  # (pool size, stride length)
+            "dense_units": 32,
+
+            "kluge_value_for_zero": 0.,  # -0.2,
+            "kluge_value_for_one": 0.,  # -0.2,
+            "aug_randomflip": True,
+            "input_noise": 10,  # in units of rgb values
+            "sample_weights_alpha": 15.0,
+            "subsample": True,
+
+            "learning_rate": 0.001,
+            "dropout": 0.10,
+            "patience": 3,
+            "batch_size": 32,
+            "max_epochs": 1_000,
+            "early_stopping": True,
+
+            "pickup_where_leftoff": False,
+            "save_best_only": True,
+        },
+
 
 
     }
